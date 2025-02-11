1d3e693a9a5535d762dcbac7247f62047a902172
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2025-02-11 14:37:56 +0530

    commit

--- technopoerfolio/src/components/Projects/Node/first.cjs
+++
@@ -1,7 +0,0 @@
-console.log("Hello");
-
-const fs = require('fs');
-fs.writeFile("output.txt", "writin File", (err) => {
-  if (err) console.log("Error occurred");
-  else console.log("File Written Successfully");
-} )
